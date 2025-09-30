522a7fb4e1a4f8b28e25c79dbb27926891e55ddb
jiordiviera <jiordikengne@gmail.com>
2025-09-30 21:36:00 +0100

    feat: Add nested resource route for tasks under projects

--- laravel-projects/task-manager/routes/api.php
+++ laravel-projects/task-manager/routes/api.php
@@ -5,4 +5,5 @@ use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::as("api")->apiResource('projects', ProjectController::class);
+
 Route::as("api")->apiResource('projects.tasks', TaskController::class);
