8a1d6fff06af71dee35a29064851299aafc9ef0c
jiordiviera <jiordikengne@gmail.com>
2025-09-30 21:16:47 +0100

    feat: Update API routes to include prefix for projects and tasks

--- laravel-projects/task-manager/routes/api.php
+++ laravel-projects/task-manager/routes/api.php
@@ -4,5 +4,5 @@ use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('projects', ProjectController::class);
-Route::apiResource('projects.tasks', TaskController::class);
+Route::prefix("api:")->apiResource('projects', ProjectController::class);
+Route::prefix("api:")->apiResource('projects.tasks', TaskController::class);
