d29f6c0679aadc6a52f7f07083d47eb1cbc5a257
jiordiviera <jiordikengne@gmail.com>
2025-09-30 01:07:29 +0100

    day 14: Production deployment of Task Manager app with complete server setup, security hardening, and service automation

--- laravel-projects/task-manager/resources/views/components/layouts/auth.blade.php
+++ laravel-projects/task-manager/resources/views/components/layouts/auth.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html
     lang="{{ str_replace("_", "-", app()->getLocale()) }}"
-    class="scroll-smooth"
+    class="scroll-smooth dark"
 >
 <head>
     <meta charset="utf-8" />
